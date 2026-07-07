c627e3ff4ddb592950ef98a686a4d2c9aad96318
Dinos Papakostas <dinospk1999@gmail.com>
2026-07-07 16:22:14 +0800

    test(config): skip permission check on windows

--- tests/test_config.py
+++ tests/test_config.py
@@ -251,6 +251,11 @@ class TestLoadSaveReset:
             os.umask(original_umask)
 
         config_path = get_config_path()
+        if os.name == "nt":
+            assert config_path.exists()
+            # Windows reports pseudo-permission bits, so we don't test them here.
+            return
+
         assert config_path.parent.stat().st_mode & 0o777 == 0o700
         assert config_path.stat().st_mode & 0o777 == 0o600
 
